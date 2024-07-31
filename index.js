87642d31333198175bbe95047b4d57096adac979
midexplorer529 <isademir237@gmail.com>
2024-07-31 22:28:35 +0300

    rotate function added to all in one and reset buttons manually

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ document.querySelector(".all-in-one").onclick = function () {
     document.querySelector(".rectangle").classList.toggle("color-active");
     setTimeout(() => document.querySelector(".rectangle").classList.toggle("square-active"), 1000);
     setTimeout(() => document.querySelector(".rectangle").classList.toggle("rotate-active"), 2000);
-    // setTimeout(() => document.querySelector(".rectangle").style.width = '300px', 2000);
+    setTimeout(() => document.querySelector(".rectangle").style.transform = `rotate(${degree}deg)`, 2000);
     // setTimeout(() => document.querySelector(".rectangle").style.heigth = '300px', 2000);
     setTimeout(() => document.querySelector(".rectangle").classList.toggle("round-active"), 3000);
 }
@@ -48,7 +48,7 @@ document.querySelector(".all-in-one").onclick = function () {
 document.querySelector(".reset").onclick = function () {
     document.querySelector(".rectangle").classList.remove("round-active");
     document.querySelector(".rectangle").classList.remove("rotate-active");
-    // setTimeout(() => document.querySelector(".rectangle").style.height = '300px', 1000);
+    document.querySelector(".rectangle").style.transform = `rotate(0deg)`;
     setTimeout(() => document.querySelector(".rectangle").classList.remove("square-active"), 1000);
     setTimeout(() => document.querySelector(".rectangle").classList.remove("color-active"), 1000);
 }
