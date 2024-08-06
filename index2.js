852520c4002c8b99f8c80d9cdefe13fe03322a9b
midexplorer529 <isademir237@gmail.com>
2024-08-06 23:34:01 +0300

    rectangle variable is added

--- index2.js
+++ index2.js
@@ -1,5 +1,6 @@
 var degree = 0;
 var color = 0;
+const rectangle = $(".rectangle");
 
 function getDegree() {
     degree = $(".degree").val();
@@ -23,26 +24,26 @@ function changeColor() {
     if (!color) {
         return;
     };
-    $(".rectangle").toggleClass("color-active");
-    $(".rectangle").css("background-color", $(".rectangle").hasClass("color-active") ? `${color}` : "#ccc");
+    rectangle.toggleClass("color-active");
+    rectangle.css("background-color", rectangle.hasClass("color-active") ? `${color}` : "#ccc");
 };
 
 $(".change-color").click(changeColor);
 
 // $(".change-color").click(function () {
 //     getColor();
-//     $(".rectangle").toggleClass("color-active");
-//     $(".rectangle").css("background-color", $(".rectangle").hasClass("color-active") ? `${color}` : "#ccc");
+//     rectangle.toggleClass("color-active");
+//     rectangle.css("background-color", rectangle.hasClass("color-active") ? `${color}` : "#ccc");
 // });
 
 function makeSquare() {
-    $(".rectangle").toggleClass("square-active");
+    rectangle.toggleClass("square-active");
 };
 
 $(".make-square").click(makeSquare);
 
 // $(".make-square").click(function () {
-//     $(".rectangle").toggleClass("square-active");
+//     rectangle.toggleClass("square-active");
 // });
 
 function rotate() {
@@ -50,62 +51,62 @@ function rotate() {
     if (!degree) {
         return;
     };
-    $(".rectangle").toggleClass("rotate-active")
-    $(".rectangle").css("transform", $(".rectangle").hasClass("rotate-active") ? `rotate(${degree}deg)` : "rotate(0deg)");
+    rectangle.toggleClass("rotate-active")
+    rectangle.css("transform", rectangle.hasClass("rotate-active") ? `rotate(${degree}deg)` : "rotate(0deg)");
 };
 
 $(".rotate").click(rotate);
 
 // $(".rotate").click(function () {
 //     getDegree();
-//     $(".rectangle").toggleClass("rotate-active")
-//     $(".rectangle").css("transform", $(".rectangle").hasClass("rotate-active") ? `rotate(${degree}deg)` : "rotate(0deg)");
+//     rectangle.toggleClass("rotate-active")
+//     rectangle.css("transform", rectangle.hasClass("rotate-active") ? `rotate(${degree}deg)` : "rotate(0deg)");
 // });
 
 function makeRound() {
-    $(".rectangle").toggleClass("round-active");
+    rectangle.toggleClass("round-active");
 };
 
 $(".make-round").click(makeRound);
 
 // $(".make-round").click(function () {
-//     $(".rectangle").toggleClass("round-active");
+//     rectangle.toggleClass("round-active");
 // });
 
 $(".all-in-one").click(function () {
     setTimeout(function () {
-        $(".rectangle").hasClass("color-active") ? null : changeColor();
+        rectangle.hasClass("color-active") ? null : changeColor();
     }, 0);
 
     setTimeout(function () {
-        $(".rectangle").hasClass("square-active") ? null : makeSquare();
+        rectangle.hasClass("square-active") ? null : makeSquare();
     }, 1000);
 
     setTimeout(function () {
-        $(".rectangle").hasClass("rotate-active") ? null : rotate();
+        rectangle.hasClass("rotate-active") ? null : rotate();
     }, 2000);
 
     setTimeout(function () {
-        $(".rectangle").hasClass("round-active") ? null : makeRound();
+        rectangle.hasClass("round-active") ? null : makeRound();
     }, 3000);
 });
 
 $(".reset").click(function () {
     setTimeout(function () {
-        $(".rectangle").removeClass("round-active");
+        rectangle.removeClass("round-active");
     }, 0);
 
     setTimeout(function () {
-        $(".rectangle").removeClass("rotate-active");
-        $(".rectangle").css("transform", "rotate(0deg)");
+        rectangle.removeClass("rotate-active");
+        rectangle.css("transform", "rotate(0deg)");
     }, 0);
 
     setTimeout(function () {
-        $(".rectangle").removeClass("square-active");
+        rectangle.removeClass("square-active");
     }, 1000);
 
     setTimeout(function () {
-        $(".rectangle").removeClass("color-active");
-        $(".rectangle").css("background-color", "#ccc");
+        rectangle.removeClass("color-active");
+        rectangle.css("background-color", "#ccc");
     }, 1000);
 });
